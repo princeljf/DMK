55b10b87c2ca9cfaf7f08cc195df5b215ac90a59
lhhformac <384234884@qq.com>
2019-08-07 17:45:52 +0800

    lhh

--- vue2x/src/views/DMK/index.js
+++ vue2x/src/views/DMK/index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./dist/dataMapsKeysMixins.js')
   } else {
-    module.exports = require('./lib/dataMapsKeysMixins.js')
+    module.exports = require('./dist/dataMapsKeysMixins.js')
 }
